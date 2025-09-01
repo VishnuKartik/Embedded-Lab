fab8f0087b7fe240f54eb7f491e65ba8670d48c5
M Vishnu Kartik <EE25MT003@iitdh.ac.in>
2025-09-02 01:18:07 +0530

    Updated the code with macro

--- main.c
+++ main.c
@@ -1,91 +1,84 @@
 #include <stdint.h>
-#include <stdbool.h>
-#include "tm4c123gh6pm.h"
+
+// Base addresses
+#define SYSCTL_RCGC2_R       (*((volatile uint32_t *)0x400FE108))
+#define GPIO_PORTF_DIR_R     (*((volatile uint32_t *)0x40025400))
+#define GPIO_PORTF_DEN_R     (*((volatile uint32_t *)0x4002551C))
+#define GPIO_PORTF_DATA_R    (*((volatile uint32_t *)0x400253FC))
+#define GPIO_PORTF_PUR_R     (*((volatile uint32_t *)0x40025510))
+#define GPIO_PORTF_LOCK_R    (*((volatile uint32_t *)0x40025520))
+#define GPIO_PORTF_CR_R      (*((volatile uint32_t *)0x40025524))
+
+// Unlock key for GPIOLOCK
+#define GPIO_LOCK_KEY        0x4C4F434B
+
+// Macro for setting/clearing GPIO bits without affecting others
+#define GPIO_PORTF_WRITE_BIT(bit_mask, value) \
+    do { \
+        if (value) \
+            GPIO_PORTF_DATA_R |= (bit_mask); \
+        else \
+            GPIO_PORTF_DATA_R &= ~(bit_mask); \
+    } while (0)
 
 int main(void)
- {
-    SYSCTL_RCGC2_R |=   0x00000020;
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R   = 0x1F;
-    GPIO_PORTF_DEN_R  = 0x1E;
-    GPIO_PORTF_DIR_R  = 0x0E;
-    GPIO_PORTF_PUR_R  = 0x10;
+{
+    volatile unsigned long delay;
+    volatile long int x;
+    long int wait = 675000;
 
-    GPIO_PORTF_DATA_R = 0x0;
-    long int x,counter=0;
-    long int delay = 675000;
+    // Enable clock to Port F
+    SYSCTL_RCGC2_R |= 0x00000020;
 
+    // Allow time for clock to stabilize
+    delay = SYSCTL_RCGC2_R;
 
-    GPIO_PORTF_DIR_R &= ~(0x11);
-    GPIO_PORTF_DEN_R |= 0x11;
+    // Unlock PF0 (it is locked by default)
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+    GPIO_PORTF_CR_R = 0x1F;           // Allow changes to PF4-0
 
+    // Enable digital functionality
+    GPIO_PORTF_DEN_R = 0x1F;
 
+    // Set PF1, PF2, PF3 as output; PF0, PF4 as input
+    GPIO_PORTF_DIR_R = 0x0E;
 
-    while (1) {
-        if ((GPIO_PORTF_DATA_R & 0x01) == 0) {
-            if ((GPIO_PORTF_DATA_R & 0x01) == 0) {
-                counter++;
+    // Enable pull-up resistors on PF0 and PF4
+    GPIO_PORTF_PUR_R = 0x11;
 
+    while (1)
+    {
+        // Both SW1 (PF4) and SW2 (PF0) pressed
+        if (((GPIO_PORTF_DATA_R & 0x01) == 0) && ((GPIO_PORTF_DATA_R & 0x10) == 0))
+        {
+            GPIO_PORTF_WRITE_BIT(0x0E, 0); // Turn off all LEDs
+            GPIO_PORTF_WRITE_BIT(0x08, 1); // Green LED on (PF3)
+        }
 
+        // Only SW1 (PF4) pressed
+        else if ((GPIO_PORTF_DATA_R & 0x10) == 0)
+        {
+            GPIO_PORTF_WRITE_BIT(0x0E, 0);
+            GPIO_PORTF_WRITE_BIT(0x02, 1); // Red LED on (PF1)
+        }
 
-                if (counter == 1) {
-                    GPIO_PORTF_DATA_R = 0x02;
-                    x=0;
-                                                  while ( x < delay)
-                                                  x ++ ;
+        // Only SW2 (PF0) pressed
+        else if ((GPIO_PORTF_DATA_R & 0x01) == 0)
+        {
+            GPIO_PORTF_WRITE_BIT(0x0E, 0);
+            GPIO_PORTF_WRITE_BIT(0x04, 1); // Blue LED on (PF2)
+        }
 
+        // No buttons pressed
+        else
+        {
+            GPIO_PORTF_WRITE_BIT(0x0E, 0); // All LEDs off
+        }
 
-                } else if (counter == 3) {
-                    GPIO_PORTF_DATA_R = 0x08;
-                    x=0;
-                                                  while ( x < delay)
-                                                  x ++ ;
+        // Delay
+        x = 0;
+        while (x < wait) x++;
+    }
 
-                } else if (counter == 2) {
-                    GPIO_PORTF_DATA_R = 0x04;
-                    x=0;
-                                                                      while ( x < delay)
-                                                                      x ++ ;
-
-                }
-                else if (counter == 4) {
-                                    GPIO_PORTF_DATA_R = 0x0A;
-                                    x=0;
-                                                                  while ( x < delay)
-                                                                  x ++ ;
-
-                                }
-                else if (counter == 5) {
-                                    GPIO_PORTF_DATA_R = 0x0C;
-                                    x=0;
-                                                                  while ( x < delay)
-                                                                  x ++ ;
-
-                                }
-                else if (counter == 6) {
-                                                    GPIO_PORTF_DATA_R = 0x06;
-                                                    x=0;
-                                                                                  while ( x < delay)
-                                                                                  x ++ ;
-
-                                                }
-
-                else if (counter == 7) {
-                                    GPIO_PORTF_DATA_R = 0x0E;
-                                    x=0;
-                                                                  while ( x < delay)
-                                                                  x ++ ;
-
-                                }
-            }}
-            else {
-                                                                                                                                GPIO_PORTF_DATA_R = 0x00; // Turn off both LEDs
-                                                                                                                                x=0;
-                                                                                                                                                   while ( x < delay)               // Delay
-                                                                                                                                             x ++ ;}
-        if (counter > 7) {
-                           counter = 0;
-                       }
-
- }
- }
+    return 0;
+}
